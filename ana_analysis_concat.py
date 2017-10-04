8965d98993213f20fcc4c1e900b68fb71e4f822b
渡辺岳人（Watanabe Gakuto） <miyabiw26@gmail.com>
2017-10-04 21:46:47 +0900

    Add files via upload

--- ana_analysis_concat.py
+++ ana_analysis_concat.py
@@ -12,21 +12,45 @@ for i in range(len(ana_df_1)):
     url = ana_df_1.ix[i,4]
     if "jpg" in str(url):
         ana_df_1=ana_df_1.drop(i)
+    elif "jpeg" in str(url):
+        ana_df_1=ana_df_1.drop(i)
     elif "png" in str(url):
         ana_df_1=ana_df_1.drop(i)
     elif "gif" in str(url):
+        ana_df_1=ana_df_1.drop(i)
+    elif "css" in str(url):
+        ana_df_1=ana_df_1.drop(i)
+    elif "js" in str(url):
+        ana_df_1=ana_df_1.drop(i)
+    elif "ico" in str(url):
+        ana_df_1=ana_df_1.drop(i)
+    elif "xml" in str(url):
+        ana_df_1=ana_df_1.drop(i)
+    elif "tmpl" in str(url):
         ana_df_1=ana_df_1.drop(i)
 
 # 読み込むエクセルファイルは１列目を削除済み
 ana_df_2=pd.read_excel("ana_df_2.xlsx")
-# URLにjpg png gifを含むものを削除
+# URLにjpg png gif cssを js含むものを削除
 for i in range(len(ana_df_2)):
     url = ana_df_2.ix[i,4]
     if "jpg" in str(url):
         ana_df_2=ana_df_2.drop(i)
+    elif "jpeg" in str(url):
+        ana_df_2=ana_df_2.drop(i)
     elif "png" in str(url):
         ana_df_2=ana_df_2.drop(i)
     elif "gif" in str(url):
+        ana_df_2=ana_df_2.drop(i)
+    elif "css" in str(url):
+        ana_df_2=ana_df_2.drop(i)
+    elif "js" in str(url):
+        ana_df_2=ana_df_2.drop(i)
+    elif "ico" in str(url):
+        ana_df_2=ana_df_2.drop(i)
+    elif "xml" in str(url):
+        ana_df_2=ana_df_2.drop(i)
+    elif "tmpl" in str(url):
         ana_df_2=ana_df_2.drop(i)
 
 # ２つのデータをまとめる
@@ -35,10 +59,12 @@ ana_df=pd.concat([ana_df_1,ana_df_2])
 number_of_access_per_day = ana_df["日付"].value_counts()
 
 series=Series(number_of_access_per_day)
-print(series.head(20))
 
 # 日付順にソート
 series_sort=series.sort_index()
 series_sort.plot()
+
+ana_df.to_excel("ana_access_log.xlsx")
+
 print(series.head(20))
 plt.show()
