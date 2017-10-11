50502c7302289ef7797e48546d1dc5c7c8733f4f
渡辺岳人（Watanabe Gakuto） <miyabiw26@gmail.com>
2017-10-11 18:50:07 +0900

    Add files via upload

--- bnlearnR.R
+++ bnlearnR.R
@@ -4,6 +4,8 @@ library(bnlearn)
 bnlearnR <- read.csv(file = "bnlearnR.csv",as.is = TRUE)
 bnlearnR<-na.omit(bnlearnR)
 
+print(dim(bnlearnR))
+
 for (i in 1:2490){
  	for(j in 1:5){
  		bnlearnR[i,j]<-as.numeric(bnlearnR[i,j])
@@ -11,25 +13,32 @@ for (i in 1:2490){
  }
 
 print(hc(bnlearnR))
-net.estimated = model2network("[SampleID][Twitter|SampleID][instagram|Twitter][redbull|Twitter:instagram][kirin|redbull]")
+net.estimated = model2network("")
 graphviz.plot(net.estimated, shape = "ellipse")
 
 bn.fit(net.estimated, bnlearnR ,method = "mle")
 
+
+
+if(0){
+
+
 library(forecast)
-training.set = protein_dataset[1:405, ]
-test.set = protein_dataset[406:810, ]
+training.set = bnlearnR[1:1245, ]
+test.set = bnlearnR[1246:2490, ]
 baysian_structure = hc(training.set)
 fitted = bn.fit(baysian_structure, training.set,method = "mle")
 test.set2 <- test.set
-test.set2$PKC <- 0
-head(test.set2)
+test.set2$redbull <- 0
+pred = predict(fitted, "redbull", test.set2)
+head(cbind(pred, test.set[, "redbull"]))
+accuracy(f = pred, x = test.set[, "redbull"])
 
-pred = predict(fitted, "PKC", test.set2)
-head(cbind(pred, test.set[, "PKC"]))
-
-accuracy(f = pred, x = test.set[, "PKC"])
-df <- data.frame(prediction=pred,actual=test.set[, "PKC"])
-p <- ggplot(df,aes(x = prediction,y = actual)) + geom_point() 
+library(ggplot2)
+df <- data.frame(prediction=pred,actual=test.set[, "redbull"])
+p <- ggplot(df,aes(x = prediction,y = actual)) + geom_point()
 p <- p + geom_line(data = data.frame(x = c(0,40), y = c(0,40)),aes(x = x, y = y), colour = "red")
 p
+
+
+}
