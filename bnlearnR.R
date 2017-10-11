75269dd582903dc8749873e734616326cd2bcdde
渡辺岳人（Watanabe Gakuto） <miyabiw26@gmail.com>
2017-10-11 11:45:10 +0900

    Add files via upload

--- bnlearnR.R
+++ bnlearnR.R
@@ -15,3 +15,21 @@ net.estimated = model2network("[SampleID][Twitter|SampleID][instagram|Twitter][r
 graphviz.plot(net.estimated, shape = "ellipse")
 
 bn.fit(net.estimated, bnlearnR ,method = "mle")
+
+library(forecast)
+training.set = protein_dataset[1:405, ]
+test.set = protein_dataset[406:810, ]
+baysian_structure = hc(training.set)
+fitted = bn.fit(baysian_structure, training.set,method = "mle")
+test.set2 <- test.set
+test.set2$PKC <- 0
+head(test.set2)
+
+pred = predict(fitted, "PKC", test.set2)
+head(cbind(pred, test.set[, "PKC"]))
+
+accuracy(f = pred, x = test.set[, "PKC"])
+df <- data.frame(prediction=pred,actual=test.set[, "PKC"])
+p <- ggplot(df,aes(x = prediction,y = actual)) + geom_point() 
+p <- p + geom_line(data = data.frame(x = c(0,40), y = c(0,40)),aes(x = x, y = y), colour = "red")
+p
