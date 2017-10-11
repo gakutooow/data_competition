e0157bb0e65ce0f6076263662a47bc464f74022e
渡辺岳人（Watanabe Gakuto） <miyabiw26@gmail.com>
2017-10-11 20:20:27 +0900

    Add files via upload

--- bnlearnR.R
+++ bnlearnR.R
@@ -6,12 +6,15 @@ bnlearnR<-na.omit(bnlearnR)
 
 print(dim(bnlearnR))
 
+# 要素を整数から実数へ変換
 for (i in 1:2490){
  	for(j in 1:5){
  		bnlearnR[i,j]<-as.numeric(bnlearnR[i,j])
  	}
  }
 
+
+#　ブラックリスト作成
 to1<- t(rep("AGE", length=15))
 to2<- t(rep("sex", length=15))
 to<-t(cbind(to1,to2))
@@ -62,23 +65,20 @@ bn.fit(net.estimated, bnlearnR ,method = "mle")
 
 
 
-/*
-library(forecast)
-training.set = bnlearnR[1:1245, ]
-test.set = bnlearnR[1246:2490, ]
-baysian_structure = hc(training.set)
-fitted = bn.fit(baysian_structure, training.set,method = "mle")
-test.set2 <- test.set
-test.set2$redbull <- 0
-pred = predict(fitted, "redbull", test.set2)
-head(cbind(pred, test.set[, "redbull"]))
-accuracy(f = pred, x = test.set[, "redbull"])
 
-library(ggplot2)
-df <- data.frame(prediction=pred,actual=test.set[, "redbull"])
-p <- ggplot(df,aes(x = prediction,y = actual)) + geom_point()
-p <- p + geom_line(data = data.frame(x = c(0,40), y = c(0,40)),aes(x = x, y = y), colour = "red")
-p
+#library(forecast)
+#training.set = bnlearnR[1:1245, ]
+#test.set = bnlearnR[1246:2490, ]
+#baysian_structure = hc(training.set)
+#fitted = bn.fit(baysian_structure, training.set,method = "mle")
+#test.set2 <- test.set
+#test.set2$redbull <- 0
+#pred = predict(fitted, "redbull", test.set2)
+#head(cbind(pred, test.set[, "redbull"]))
+#accuracy(f = pred, x = test.set[, "redbull"])
 
-
-*/
+#library(ggplot2)
+#df <- data.frame(prediction=pred,actual=test.set[, "redbull"])
+#p <- ggplot(df,aes(x = prediction,y = actual)) + geom_point()
+#p <- p + geom_line(data = data.frame(x = c(0,40), y = c(0,40)),aes(x = x, y = y), colour = "red")
+#p
