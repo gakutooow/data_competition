10797fa541ba96b370e72d3e6295419f29bf3c35
渡辺岳人（Watanabe Gakuto） <miyabiw26@gmail.com>
2017-10-12 22:44:05 +0900

    Add files via upload

--- polcaR.R
+++ polcaR.R
@@ -1,6 +1,10 @@
+library(poLCA)
+
+#values.csvは消費価値観だけ取り出したメインデータ
 values <- read.csv(file = "values.csv",as.is = TRUE)
 values<-na.omit(values)
 
+#値が自然数しかダメなので01から12データに変換
 for(i in 1:2854){
   for(j in 1:33){
     if(values[i,j] == 1){
@@ -11,13 +15,16 @@ for(i in 1:2854){
   }
 }
 
-
+#nameにデータフレームのカラム名を入れる
 name<-names(values)
+
+#知恵袋的なところで見つけたコード。これやるとうまくいった。http://r.789695.n4.nabble.com/cbind-formula-definition-td903703.html
 f <- as.formula(paste("cbind(", paste(name, collapse = ","), ")~1"))
 
-
+#BIC（情報量基準）の最小値の初期値
 min_bic <- 100000
 
+#クラスを２から７まで変えていって、一番いいモデルを出力
 for(i in 2:7){
   lc <- poLCA(f, values, nclass=i, maxiter=3000,
               tol=1e-5, na.rm=FALSE,
